e2230fac02c9524be67e603987423378a1e8ab69
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2014-04-07 12:00:00 -0700

    New port Refocus 0.9.0: GIMP plugin to "refocus" images through FIR WIENER filtering
    FreeBSD-ports - Obtained from:	gentoo portage (patches).
    Mario Sergio Fujikawa Ferreira authored and
    Mario Sergio Fujikawa Ferreira committed on Apr 7, 2014

--- src/gimppreview.c
+++ src/gimppreview.c
@@ -36,18 +36,18 @@ static char vcid[] = "$Id: gimppreview.c,v 1.2 2004/05/13 18:20:48 ernstl Exp $"
 #include "gimppreview.h"
 
 
-static void gimp_preview_init (GimpPreview * preview);
-static void gimp_preview_class_init (GimpPreviewClass * klass);
+static void gimp_preview_init (myGimpPreview * preview);
+static void gimp_preview_class_init (myGimpPreviewClass * klass);
 
 static void gimp_preview_plus_callback (GtkWidget * widget, gpointer data);
 static void gimp_preview_minus_callback (GtkWidget * widget, gpointer data);
 static gint gimp_preview_event (GtkWidget * widget, GdkEvent * event,
                                 gpointer data);
-static void gimp_preview_recompute_sizes (GimpPreview * preview,
+static void gimp_preview_recompute_sizes (myGimpPreview * preview,
                                           gdouble newscale);
-static void gimp_preview_update_preview (GimpPreview * preview);
+static void gimp_preview_update_preview (myGimpPreview * preview);
 
-static void gimp_preview_image_set_size (GimpPreview * preview, gint width,
+static void gimp_preview_image_set_size (myGimpPreview * preview, gint width,
                                          gint height);
 static void gimp_preview_size_request (GtkWidget * widget,
                                        GtkRequisition * requisition);
@@ -58,7 +58,7 @@ static void gimp_preview_forall (GtkContainer * container,
                                  GtkCallback callback,
                                  gpointer callback_data);
 gboolean gimp_preview_update_preview_idle_fun (gpointer data);
-void gimp_preview_schedule_update (GimpPreview * preview);
+void gimp_preview_schedule_update (myGimpPreview * preview);
 void gimp_preview_realized_signal_handler(GtkWidget *widget, gpointer user_data);
 
 #define PROGRESS_BAR_HEIGHT (10)
@@ -95,11 +95,11 @@ static guint gimp_preview_signals[LAST_SIGNAL] = { 0 };
 
 
 /*
- * Apps which use a GimpPreview widget should not be accessing the private
+ * Apps which use a myGimpPreview widget should not be accessing the private
  * data!
  */
 #define PREVIEW_DATA(preview) \
-        ((GimpPreviewData*)(GIMP_PREVIEW (preview)->private_data))
+        ((myGimpPreviewData*)(MY_GIMP_PREVIEW (preview)->private_data))
 
 typedef struct
 {
@@ -127,7 +127,7 @@ typedef struct
   GtkWidget *image;
   GtkWidget *progress_bar;
 }
-GimpPreviewData;
+myGimpPreviewData;
 
 
 /* Convert coordinate in preview space to image coordinates */
@@ -156,20 +156,20 @@ gimp_preview_get_type (void)
   if (!preview_type)
     {
       GTypeInfo preview_info = {
-        sizeof (GimpPreviewClass),
+        sizeof (myGimpPreviewClass),
         (GBaseInitFunc) NULL,
         (GBaseFinalizeFunc) NULL,
         (GClassInitFunc) gimp_preview_class_init,
         (GClassFinalizeFunc) NULL,
         (gconstpointer) NULL,   /* class_data */
-        sizeof (GimpPreview),
+        sizeof (myGimpPreview),
         0,                      /* n_preallocs */
         (GInstanceInitFunc) gimp_preview_init,
         (GTypeValueTable *) NULL /* value_table */
       };
 
       preview_type =
-        g_type_register_static (GTK_TYPE_CONTAINER, "GimpPreview",
+        g_type_register_static (GTK_TYPE_CONTAINER, "myGimpPreview",
                                 &preview_info, 0);
     }
 
@@ -182,7 +182,7 @@ gimp_preview_get_type (void)
  * by GTK's internal mechanisms.
  */
 static void
-gimp_preview_class_init (GimpPreviewClass * klass)
+gimp_preview_class_init (myGimpPreviewClass * klass)
 {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
   GtkContainerClass *container_class = GTK_CONTAINER_CLASS (klass);
@@ -191,7 +191,7 @@ gimp_preview_class_init (GimpPreviewClass * klass)
     g_signal_new ("update_preview",
                   G_TYPE_FROM_CLASS (klass),
                   G_SIGNAL_RUN_FIRST,
-                  G_STRUCT_OFFSET (GimpPreviewClass, update_preview),
+                  G_STRUCT_OFFSET (myGimpPreviewClass, update_preview),
                   NULL,
                   NULL,
                   g_cclosure_marshal_VOID__POINTER,
@@ -201,7 +201,7 @@ gimp_preview_class_init (GimpPreviewClass * klass)
     g_signal_new ("preview_changed",
                   G_TYPE_FROM_CLASS (klass),
                   G_SIGNAL_RUN_FIRST,
-                  G_STRUCT_OFFSET (GimpPreviewClass, preview_changed),
+                  G_STRUCT_OFFSET (myGimpPreviewClass, preview_changed),
                   NULL, NULL, g_cclosure_marshal_VOID__VOID, G_TYPE_NONE, 0);
 
   klass->update_preview = NULL;
@@ -213,7 +213,7 @@ gimp_preview_class_init (GimpPreviewClass * klass)
 
 
 void
-gimp_preview_set_scale_amount(GimpPreview *preview, gdouble scale_amount)
+gimp_preview_set_scale_amount(myGimpPreview *preview, gdouble scale_amount)
 {
   /*
    * If the caller wants to set the scale amount, let them do so.
@@ -276,7 +276,7 @@ void gimp_preview_realized_signal_handler(GtkWidget *widget, gpointer preview){
  * by GTK's internal mechanisms.
  */
 static void
-gimp_preview_init (GimpPreview * preview)
+gimp_preview_init (myGimpPreview * preview)
 {
   gchar buffer[10];
 
@@ -284,7 +284,7 @@ gimp_preview_init (GimpPreview * preview)
   GTK_WIDGET_SET_FLAGS (preview, GTK_NO_WINDOW);
   GTK_CONTAINER (preview)->resize_mode = GTK_RESIZE_IMMEDIATE;
 
-  preview->private_data = g_malloc0 (sizeof (GimpPreviewData));
+  preview->private_data = g_malloc0 (sizeof (myGimpPreviewData));
 
 
   PREVIEW_DATA (preview)->label = gtk_label_new ("");
@@ -314,7 +314,7 @@ GtkWidget *
 gimp_preview_new_with_args (GimpDrawable * drawable, gint cb_preview_size,
                             gdouble cb_scale_amount, gint cb_allow_scale)
 {
-  GimpPreview *preview;
+  myGimpPreview *preview;
   GtkWidget *frame;
   GtkWidget *hbox;
   GtkWidget *event_box;
@@ -344,14 +344,14 @@ gimp_preview_new_with_args (GimpDrawable * drawable, gint cb_preview_size,
 
 
   /* Now allocate the actual preview window. */
-  preview = GIMP_PREVIEW (g_object_new (gimp_preview_get_type (), NULL));
+  preview = MY_GIMP_PREVIEW (g_object_new (gimp_preview_get_type (), NULL));
 
   /* Set the scale amount. */
   gimp_preview_set_scale_amount(preview, cb_scale_amount);
 
   /* Save the drawable info. */
   preview->drawable = drawable;
-  preview->drawable_has_alpha = gimp_drawable_has_alpha (drawable->id);
+  preview->drawable_has_alpha = gimp_drawable_has_alpha (drawable->drawable_id);
 
   /* Calculate our preview size. */
   if (preview_size == PREVIEW_FIXED_SIZE)
@@ -359,8 +359,8 @@ gimp_preview_new_with_args (GimpDrawable * drawable, gint cb_preview_size,
       preview_width = i2p (drawable->width, preview->scale);
       preview_height = i2p (drawable->height, preview->scale);
 
-      GIMP_PREVIEW (preview)->width = preview_width;
-      GIMP_PREVIEW (preview)->height = preview_height;
+      MY_GIMP_PREVIEW (preview)->width = preview_width;
+      MY_GIMP_PREVIEW (preview)->height = preview_height;
     }
   else
     {
@@ -465,7 +465,7 @@ gimp_preview_new_with_args (GimpDrawable * drawable, gint cb_preview_size,
 static void
 gimp_preview_size_request (GtkWidget * widget, GtkRequisition * requisition)
 {
-  GimpPreview *preview = GIMP_PREVIEW (widget);
+  myGimpPreview *preview = MY_GIMP_PREVIEW (widget);
   GtkRequisition resize_box_requisition;
 
 #ifdef PREVIEW_DEBUG
@@ -489,7 +489,7 @@ gimp_preview_size_request (GtkWidget * widget, GtkRequisition * requisition)
 static void
 gimp_preview_size_allocate (GtkWidget * widget, GtkAllocation * allocation)
 {
-  GimpPreview *preview = GIMP_PREVIEW (widget);
+  myGimpPreview *preview = MY_GIMP_PREVIEW (widget);
   GtkAllocation resize_box_allocation, progress_bar_allocation,
     event_box_allocation;
   GtkRequisition resize_box_requisition;
@@ -554,7 +554,7 @@ gimp_preview_forall (GtkContainer * container,
                      gboolean include_internals,
                      GtkCallback callback, gpointer callback_data)
 {
-  GimpPreview *preview = GIMP_PREVIEW (container);
+  myGimpPreview *preview = MY_GIMP_PREVIEW (container);
 
   if (PREVIEW_DATA (preview)->resize_box)
     {
@@ -575,7 +575,7 @@ gimp_preview_forall (GtkContainer * container,
  * Plug-ins call this to do an update of the preview area.
  */
 void
-gimp_preview_update (GimpPreview * preview)
+gimp_preview_update (myGimpPreview * preview)
 {
   gimp_preview_recompute_sizes (preview, preview->scale);
   gimp_preview_update_preview (preview);
@@ -590,11 +590,11 @@ gimp_preview_update (GimpPreview * preview)
 static void
 gimp_preview_plus_callback (GtkWidget * widget, gpointer data)
 {
-  GimpPreview *preview;
+  myGimpPreview *preview;
   gchar buffer[10];
   gdouble new_scale;
 
-  preview = GIMP_PREVIEW (data);
+  preview = MY_GIMP_PREVIEW (data);
   if (PREVIEW_DATA (preview)->scale_n == PREVIEW_SCALE_LAST)
     return;
 
@@ -627,11 +627,11 @@ gimp_preview_plus_callback (GtkWidget * widget, gpointer data)
 static void
 gimp_preview_minus_callback (GtkWidget * widget, gpointer data)
 {
-  GimpPreview *preview;
+  myGimpPreview *preview;
   gchar buffer[10];
   gdouble new_scale;
 
-  preview = GIMP_PREVIEW (data);
+  preview = MY_GIMP_PREVIEW (data);
   if (PREVIEW_DATA (preview)->scale_n == 0)
     return;
 
@@ -662,12 +662,12 @@ gimp_preview_minus_callback (GtkWidget * widget, gpointer data)
 static gint
 gimp_preview_event (GtkWidget * widget, GdkEvent * event, gpointer data)
 {
-  GimpPreview *preview;
+  myGimpPreview *preview;
   GdkEventButton *button_event;
   gint x, y;
   gint dx, dy;
 
-  preview = GIMP_PREVIEW (data);
+  preview = MY_GIMP_PREVIEW (data);
   button_event = (GdkEventButton *) event;
 
   switch (event->type)
@@ -744,7 +744,7 @@ gimp_preview_event (GtkWidget * widget, GdkEvent * event, gpointer data)
  * This function is also used for initializing the preview.
  */
 static void
-gimp_preview_recompute_sizes (GimpPreview * preview, gdouble new_scale)
+gimp_preview_recompute_sizes (myGimpPreview * preview, gdouble new_scale)
 {
 
   /* The center of the preview in image coordinates.
@@ -776,7 +776,7 @@ gimp_preview_recompute_sizes (GimpPreview * preview, gdouble new_scale)
 }
 
 void
-gimp_preview_generate_update_event (GimpPreview * preview)
+gimp_preview_generate_update_event (myGimpPreview * preview)
      /* Signal the user that the preview must be updated */
 {
   const gdouble scale = preview->scale;
@@ -794,7 +794,7 @@ gimp_preview_generate_update_event (GimpPreview * preview)
            0,
            preview->drawable->height - image_y);
 
-  GimpPreviewEvent preview_event;
+  myGimpPreviewEvent preview_event;
 
   preview_event.event_id = PREVIEW_DATA (preview)->current_event_id;
   preview_event.scale = preview->scale;
@@ -825,7 +825,7 @@ gimp_preview_generate_update_event (GimpPreview * preview)
  * to step through source and destination!
  */
 static void
-gimp_preview_update_preview (GimpPreview * preview)
+gimp_preview_update_preview (myGimpPreview * preview)
 {
   GimpPixelRgn region;
   guchar *image_data = NULL;
@@ -1023,7 +1023,7 @@ gimp_preview_update_preview (GimpPreview * preview)
 
 
 void
-gimp_preview_force_redraw (GimpPreview * preview)
+gimp_preview_force_redraw (myGimpPreview * preview)
 {
   gtk_widget_queue_draw (GTK_WIDGET (PREVIEW_DATA (preview)->image));
 }
@@ -1033,7 +1033,7 @@ gimp_preview_force_redraw (GimpPreview * preview)
 gboolean
 gimp_preview_update_preview_idle_fun (gpointer data)
 {
-  GimpPreview *preview = GIMP_PREVIEW (data);
+  myGimpPreview *preview = MY_GIMP_PREVIEW (data);
   gint event_id = PREVIEW_DATA (preview)->current_event_id;
 
 #ifdef PREVIEW_DEBUG
@@ -1052,7 +1052,7 @@ gimp_preview_update_preview_idle_fun (gpointer data)
 }
 
 void
-gimp_preview_schedule_update (GimpPreview * preview)
+gimp_preview_schedule_update (myGimpPreview * preview)
 {
   PREVIEW_DATA (preview)->current_event_id++;
 
@@ -1093,7 +1093,7 @@ gimp_preview_image_draw_row (GtkWidget * image, guchar * data,
 }
 
 void
-gimp_preview_image_set_size (GimpPreview * preview, gint width, gint height)
+gimp_preview_image_set_size (myGimpPreview * preview, gint width, gint height)
 {
   const gint real_width = MIN (preview->max_width, width);
   const gint real_height = MIN (preview->max_height, height);
@@ -1131,8 +1131,8 @@ gimp_preview_image_set_size (GimpPreview * preview, gint width, gint height)
 
 /**
  * gimp_preview_draw_row:
- * @preview: the #GimpPreview
- * @event_id: event_id that was sent with the #GimpPreviewEvent.
+* @preview: the #myGimpPreview
+ * @event_id: event_id that was sent with the #myGimpPreviewEvent.
  * @type: the format of the data (e.g. %GIMP_RGBA_IMAGE).
  * @row:the relative number of the row within the preview.
  * The top row of the preview is number 0.
@@ -1148,7 +1148,7 @@ gimp_preview_image_set_size (GimpPreview * preview, gint width, gint height)
  * with the same event-id will be ignored by the preview.
  **/
 gboolean
-gimp_preview_draw_row (GimpPreview * preview, const gint event_id,
+gimp_preview_draw_row (myGimpPreview * preview, const gint event_id,
                        GimpImageType type, gint row,
                        const guchar * const data)
 {
@@ -1258,8 +1258,8 @@ gimp_preview_draw_row (GimpPreview * preview, const gint event_id,
 
 /**
  * gimp_preview_draw_unscaled_row:
- * @preview: the #GimpPreview
- * @event_id: event_id that was sent with the #GimpPreviewEvent.
+ * @preview: the #myGimpPreview
+ * @event_id: event_id that was sent with the #myGimpPreviewEvent.
  * @type: the format of the data (e.g. %GIMP_RGBA_IMAGE).
  * @row:row is the relative position of the row w.r.t. preview_event->image_y.
  * The top row has number 0.
@@ -1278,7 +1278,7 @@ gimp_preview_draw_row (GimpPreview * preview, const gint event_id,
  * with the same event-id will be ignored by the preview.
  **/
 gboolean
-gimp_preview_draw_unscaled_row (GimpPreview * preview, const gint event_id,
+gimp_preview_draw_unscaled_row (myGimpPreview * preview, const gint event_id,
                                 GimpImageType type, const gint row,
                                 const guchar * const data)
 {
@@ -1456,8 +1456,8 @@ gimp_preview_draw_unscaled_row (GimpPreview * preview, const gint event_id,
 
 /**
  * gimp_preview_progress_set_fraction:
- * @preview: the #GimpPreview.
- * @event_id: event_id that was sent with the #GimpPreviewEvent.
+ * @preview: the #myGimpPreview.
+ * @event_id: event_id that was sent with the #myGimpPreviewEvent.
  * @fraction: the fraction completed.
  *
  * Set the progress bar of the preview to @fraction completed.
@@ -1467,7 +1467,7 @@ gimp_preview_draw_unscaled_row (GimpPreview * preview, const gint event_id,
  * with the same event-id will be ignored by the preview.
  **/
 gboolean
-gimp_preview_progress_set_fraction (GimpPreview * preview,
+gimp_preview_progress_set_fraction (myGimpPreview * preview,
                                     const gint event_id, double fraction)
 {
   const gboolean return_status =
--- src/refocus.c
+++ src/refocus.c
@@ -45,9 +45,9 @@ GimpDrawable *drawable;
 
 /* Declare local functions. */
 static void query (void);
-static void run (char *name,
+static void run (const gchar *name,
                  gint nparams,
-                 GimpParam * param,
+                 const GimpParam * param,
                  gint * nreturn_vals, GimpParam ** return_vals);
 static gint dialog ();
 static void doit (void);
@@ -98,7 +98,8 @@ my_widgets;
 
 
 MAIN ()
-     static void query ()
+
+static void query ()
 {
   static GimpParamDef args[] = {
     {GIMP_PDB_INT32, "run_mode", "Interactive, non-interactive"},
@@ -111,7 +112,6 @@ MAIN ()
     {GIMP_PDB_FLOAT, "noise", "Noise to Signal ratio"},
   };
   static GimpParamDef *return_vals = NULL;
-  static gint nargs = (gint) (sizeof (args) / sizeof (args[0]));
   static gint nreturn_vals = 0;
 
   gimp_install_procedure ("plug_in_refocus",
@@ -123,15 +123,15 @@ MAIN ()
                           "<Image>/Filters/Enhance/Refocus ...",
                           "RGB*, GRAY*",
                           GIMP_PLUGIN,
-                          nargs, nreturn_vals, args, return_vals);
+                          G_N_ELEMENTS(args), nreturn_vals, args, return_vals);
 }
 
 static void
-run (char *name, gint n_params, GimpParam * param,
-     gint * nreturn_vals, GimpParam ** return_vals)
+run (const gchar *name, gint n_params, const GimpParam *param,
+     gint *nreturn_vals, GimpParam **return_vals)
 {
   static GimpParam values[1];
-  GimpRunModeType run_mode;
+  GimpRunMode run_mode;
   GimpPDBStatusType status = GIMP_PDB_SUCCESS;
 
   (void) name;                  /* Shut up warnings about unused parameters. */
@@ -186,8 +186,8 @@ run (char *name, gint n_params, GimpParam * param,
     {
 
       /*  Make sure that the drawable is gray or RGB color  */
-      if (gimp_drawable_is_rgb (drawable->id) ||
-          gimp_drawable_is_gray (drawable->id))
+      if (gimp_drawable_is_rgb (drawable->drawable_id) ||
+          gimp_drawable_is_gray (drawable->drawable_id))
         {
           doit ();
 
@@ -336,7 +336,7 @@ update_callback (GtkWidget * widget, gpointer data)
   (void) data;
   set_busy_cursor (widget, TRUE);
   update_matrix ();
-  gimp_preview_update (GIMP_PREVIEW (my_widgets.preview));
+  gimp_preview_update (MY_GIMP_PREVIEW (my_widgets.preview));
   set_busy_cursor (widget, FALSE);
 }
 
@@ -369,11 +369,11 @@ preview_progress_update_fun (const gpointer data, gdouble arg)
 {
   gint event_id = GPOINTER_TO_INT (data);
   return (gimp_preview_progress_set_fraction
-          (GIMP_PREVIEW (my_widgets.preview), event_id, arg));
+          (MY_GIMP_PREVIEW (my_widgets.preview), event_id, arg));
 }
 
 static void
-preview_callback (GtkWidget * widget, GimpPreviewEvent * event, gpointer data)
+preview_callback (GtkWidget * widget, myGimpPreviewEvent * event, gpointer data)
 {
   TileSource source;
   TileSink sink;
@@ -393,7 +393,7 @@ preview_callback (GtkWidget * widget, GimpPreviewEvent * event, gpointer data)
                                       im_width, im_height);
       tile_sink_init_for_preview (&sink, drawable, image_x, image_y,
                                   im_width, im_height);
-      gimp_preview_progress_set_fraction (GIMP_PREVIEW (my_widgets.preview),
+      gimp_preview_progress_set_fraction (MY_GIMP_PREVIEW (my_widgets.preview),
                                           event->event_id, 0);
       bd_closure_init (&update_progress_closure,
                        preview_progress_update_fun,
@@ -409,9 +409,9 @@ preview_callback (GtkWidget * widget, GimpPreviewEvent * event, gpointer data)
         {
           tile_sink_get_row (&sink, buf, image_x, image_y + row, im_width);
           event_is_current =
-            gimp_preview_draw_unscaled_row (GIMP_PREVIEW (my_widgets.preview),
+            gimp_preview_draw_unscaled_row (MY_GIMP_PREVIEW (my_widgets.preview),
                                             event->event_id,
-                                            gimp_drawable_type (drawable->id),
+                                            gimp_drawable_type (drawable->drawable_id),
                                             row, buf);
         };
       g_free (buf);
@@ -588,7 +588,7 @@ dialog ()
 
   gtk_widget_show (dlg);
   redraw_all ();
-  gimp_preview_update (GIMP_PREVIEW (preview));
+  gimp_preview_update (MY_GIMP_PREVIEW (preview));
   gtk_main ();
   gdk_flush ();
   return run_flag;
@@ -613,7 +613,7 @@ doit (void)
   gimp_progress_init ("Computing matrix");
   update_matrix ();
   gimp_progress_init ("Applying convolution");
-  gimp_drawable_mask_bounds (drawable->id, &sx1, &sy1, &sx2, &sy2);
+  gimp_drawable_mask_bounds (drawable->drawable_id, &sx1, &sy1, &sx2, &sy2);
   width = sx2 - sx1;
   height = sy2 - sy1;
   tile_source_init_from_drawable (&source, drawable, sx1, sy1, width, height);
@@ -623,7 +623,7 @@ doit (void)
                   matrix, 2 * my_config.mat_width + 1,
                   &update_progress_closure);
   gimp_drawable_flush (drawable);
-  gimp_drawable_merge_shadow (drawable->id, TRUE);
-  gimp_drawable_update (drawable->id, sx1, sy1, width, height);
+  gimp_drawable_merge_shadow (drawable->drawable_id, TRUE);
+  gimp_drawable_update (drawable->drawable_id, sx1, sy1, width, height);
   g_free (matrix);
 }
